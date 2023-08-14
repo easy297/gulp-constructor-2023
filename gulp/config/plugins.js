8bbb6e24b4923d3bfb629beb4baf8cdb7b4bff49
easy297 <saybosyn.77@gmail.com>
2023-08-14 18:56:16 +0600

    Finished images.js

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -3,11 +3,13 @@ import plumber from "gulp-plumber"; // Notify errors
 import notify from "gulp-notify"; // Messages (tips)
 import browserSync from 'browser-sync'; // Local Server
 import ifPlugin from "gulp-if";
+import newer from "gulp-newer"; // Changes tracker
 
 export const plugins = {
     replace : replace,
     plumber: plumber,
     notify: notify,
     browserSync: browserSync,
-    if: ifPlugin
+    if: ifPlugin,
+    newer: newer
 }
--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,11 @@ import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
+import {images} from "./gulp/tasks/images.js";
+// import {otfToTtf, ttfToWoff, fontsStyle} from "./gulp/tasks/fonts.js";
+// import {svgSprive} from "./gulp/tasks/svgSprive.js";
+// import {zip} from "./gulp/tasks/zip.js";
+// import {ftp} from "./gulp/tasks/ftp.js";
 
 // Function to watch changes in files
 function watcher() {
@@ -25,9 +30,10 @@ function watcher() {
     gulp.watch(path.watch.html, html)
     gulp.watch(path.watch.scss, scss)
     gulp.watch(path.watch.js, js)
+    gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js);
+const mainTasks = gulp.parallel(copy, html, scss, js, images);
 
 // Running tasks
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
