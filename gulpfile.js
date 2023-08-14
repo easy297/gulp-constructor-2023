33c6571d9bca08424b311d740e58b45a4ad36eb9
easy297 <saybosyn.77@gmail.com>
2023-08-14 19:42:11 +0600

    Finished fonts.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import {images} from "./gulp/tasks/images.js";
-// import {otfToTtf, ttfToWoff, fontsStyle} from "./gulp/tasks/fonts.js";
+import {otfToTtf, ttfToWoff, fontsStyle} from "./gulp/tasks/fonts.js";
 // import {svgSprive} from "./gulp/tasks/svgSprive.js";
 // import {zip} from "./gulp/tasks/zip.js";
 // import {ftp} from "./gulp/tasks/ftp.js";
@@ -33,7 +33,10 @@ function watcher() {
     gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 // Running tasks
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
